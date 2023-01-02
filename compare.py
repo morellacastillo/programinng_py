47221e7c92f739526db010b6d432fb8c0bdc6e0e
morellacastillo <morellacastillomirich@gmail.com>
2023-01-02 20:12:03 -0300

    actualization

---
+++ compare.py
@@ -0,0 +1,8 @@
+x=int(input("What's variable x? "))
+y=int(input("What's variable y? "))
+
+if x != y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+    
--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 #creating a function
 def hello(to="world"):
     print("Hello, ", to)
+
 hello()
 name=input ("What's your name? ")
 print (hello(name))
